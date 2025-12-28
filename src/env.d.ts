921f6298fef095c1ea0cc85b3f6e095be6cada30
Wix Remote Machine <support@wix.com>
2025-12-28 11:16:52 +0000

    Manual code changes

--- src/env.d.ts
+++ src/env.d.ts
@@ -5,5 +5,5 @@ declare global {
     strict: true;
   }
 }
-VITE_SUPABASE_URL=https://xlymenigknxuhwgfjfmw.supabase.co
+VITE_SUPABASE_URL=https://xlymenigknxuhwgfjfmw.supabase.co;
 VITE_SUPABASE_ANON_KEY=eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InhseW1lbmlna254dWh3Z2ZqZm13Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NjY5MTc2NDcsImV4cCI6MjA4MjQ5MzY0N30.ca1dpVdZ2LABESo-v76hS1vwu8Lhd6w3Iy3fwY9MSOo
